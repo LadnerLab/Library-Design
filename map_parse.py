a5576c49672be416897e103f1c7d02a7455e7cf2
Bob Costas <finkzane@gmail.com>
2018-10-01 16:09:54 -0700

    added program options/arguments

--- map_parse.py
+++ map_parse.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
+import argparse
+
+import protein_oligo_library as oligo
 
 def main():
-    return 0
+    arg_parser = argparse.ArgumentParser( description = "Script to parse information given by validate_design's epitope map output" )
 
+    arg_parser.add_argument( '-m', '--map', help = "Name of file containing epitope map" )
+    arg_parser.add_argument( '-o', '--output', default = "parsed_map.txt",
+                             help = (
+                                      "Name of file to write parsed output to."
+                                      " Output will be written in a tab-delimited format"
+                                    )
+                           )
+    arg_parser.add_argument( '-t', '--tax_db',
+                             help = "Name of file containing mappings of taxonomic id -> rank data"
+                           )
+
+    args = arg_parser.parse_args()
 
 if __name__ == '__main__':
     main()
