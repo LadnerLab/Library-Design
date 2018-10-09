7e6f223f3b88617c6601c619336166778316521d
Bob Costas <finkzane@gmail.com>
2018-10-09 15:15:14 -0700

    added the ability for the line to be split up

--- fill_tax_gaps.py
+++ fill_tax_gaps.py
@@ -5,19 +5,34 @@ def main():
 
     nodes_dmp   = sys.argv[ 1 ]
     lineage_dmp = sys.argv[ 2 ]
+    out_file    = sys.argv[ 3 ]
 
     nodes_dict = parse_nodes( nodes_dmp )
 
 
 def parse_nodes( file_name ):
-    READ_ONLY_FLAG = "r"
+    READ_ONLY_FLAG = 'r'
+    DELIMITER_CHAR = '|'
     nodes_file     =  open( file_name,
                             READ_ONLY_FLAG
                           )
     nodes_dict = {}
 
+    current_id        = 0
+    current_parent_id = 0
+    current_rank      = ""
+
     for line in nodes_file:
-        continue
+        split_line = [ item.strip() \
+                       for item in \
+                       line.split( DELIMITER_CHAR )
+                     ]  
+
+        current_id        = int( split_line[ 0 ] )
+        current_parent_id = int( split_line[ 1 ] )
+        current_rank      = split_line[ 2 ]
+
+
 
     nodes_file.close()
 
