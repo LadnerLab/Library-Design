bd644de2f828fa5be991c114504ede68837b09a4
Bob Costas <finkzane@gmail.com>
2018-10-09 14:59:27 -0700

    added skeleton for parse_nodes

--- fill_tax_gaps.py
+++ fill_tax_gaps.py
@@ -6,7 +6,22 @@ def main():
     nodes_dmp   = sys.argv[ 1 ]
     lineage_dmp = sys.argv[ 2 ]
 
+    nodes_dict = parse_nodes( nodes_dmp )
 
+
+def parse_nodes( file_name ):
+    READ_ONLY_FLAG = "r"
+    nodes_file     =  open( file_name,
+                            READ_ONLY_FLAG
+                          )
+    nodes_dict = {}
+
+    for line in nodes_file:
+        continue
+
+    nodes_file.close()
+
+    return nodes_dict
 
 if __name__ == '__main__':
     main()
