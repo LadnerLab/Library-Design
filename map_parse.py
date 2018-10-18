cf1558abdc59cd55bb0a78b8d4038f5b977058f6
jtladner <jtladner@gmail.com>
2018-10-18 11:56:43 -0700

    Taxonomic tree fix

--- map_parse.py
+++ map_parse.py
@@ -53,7 +53,7 @@ def main():
         sys.exit( 1 )
 
     try:
-        tax_dict, taxid_dict = oligo_to_tax( map_dict, args.tax_db )
+        tax_dict, taxid_dict = oligo_to_tax( map_dict, args.tax_db, gap_dict )
         sequence_dict        = oligo.sequence_dict_from_file( args.reference )
     except ( IOError, OSError ):
         print( "ERROR: An IO exception occurred when trying "
@@ -69,11 +69,11 @@ def main():
     # By this point, our data can be safely assumed as valid,
     #so we don't have to do any more verification
 
-    species_centric_table  = create_species_centric_table( map_dict, taxid_dict, oligo_seq_dict, tax_dict, gap_dict )
-    oligo_centric_table    = create_oligo_centric_table( tax_dict, map_dict, taxid_dict, gap_dict )
-    sequence_centric_table = create_sequence_centric_table_from_oligos( epitope_dict, oligo_seq_dict, gap_dict )
+#    species_centric_table  = create_species_centric_table( map_dict, taxid_dict, oligo_seq_dict, tax_dict)
+    oligo_centric_table    = create_oligo_centric_table( tax_dict, map_dict, taxid_dict)
+    sequence_centric_table = create_sequence_centric_table_from_oligos( epitope_dict, oligo_seq_dict)
 
-    create_sequence_centric_table( sequence_dict, epitope_dict, gap_dict )
+#    create_sequence_centric_table( sequence_dict, epitope_dict)
 
     write_outputs( args.output, oligo_centric_table, sequence_centric_table )
     
@@ -120,28 +120,28 @@ def parse_map( file_name ):
                 split_line = line.split( TAB_CHAR )
                 # Information for oligo-centric data
                 new_dict_key = split_line[ 0 ]
-                new_dict_val = split_line[ 1 ]
+                new_dict_val = split_line[ 1 ].strip()
 
                 #information for sequence-centric data
                 sequence_dict_key = remove_loc_markers( split_line[ 0 ] )
 
-                if sequence_dict_key in oligo_seq_dict.keys():
+                if sequence_dict_key in oligo_seq_dict:
                     oligo_seq_dict[ sequence_dict_key ] += 1
                 else:
                     oligo_seq_dict[ sequence_dict_key ] = 1
 
-                for current_item in split_line[ 1 ].split( DELIMITER_CHAR ):
+                for current_item in split_line[ 1 ].strip().split( DELIMITER_CHAR ):
                     if current_item in oligo_seq_dict.keys():
                         oligo_seq_dict[ current_item ] += 1
                     else:
                         oligo_seq_dict[ current_item ] = 1
 
-                if sequence_dict_key in seq_dict.keys():
+                if sequence_dict_key in seq_dict:
                     current_entry = seq_dict[ sequence_dict_key ]
                     current_entry[ 0 ] += 1
-                    current_entry[ 1 ] += len( split_line[ 1 ].split( DELIMITER_CHAR ) )
+                    current_entry[ 1 ] += len( split_line[ 1 ].strip().split( DELIMITER_CHAR ) )
                 else:
-                    seq_dict[ sequence_dict_key ] = [ 1, len( split_line[ 1 ].split( DELIMITER_CHAR ) ) ]
+                    seq_dict[ sequence_dict_key ] = [ 1, len( split_line[ 1 ].strip().split( DELIMITER_CHAR ) ) ]
 
             except ( IndexError ):
                 raise InputFormatFileError
@@ -152,7 +152,7 @@ def parse_map( file_name ):
             
             # String is formatted correctly 
             else:
-                if new_dict_key in oligo_dict.keys():
+                if new_dict_key in oligo_dict:
                     oligo_dict[ new_dict_key ] = oligo_dict[ new_dict_key ] + \
                                                 new_dict_val.strip().split( DELIMITER_CHAR )
                 else:
@@ -174,6 +174,20 @@ def oligo_to_tax( input_dict, tax_data_file, gap_dict = None ):
     output_dict = {}
     taxid_dict = oligo.get_taxdata_from_file( tax_data_file )
 
+    # "Fix" taxid_dict using gap_dict
+    if gap_dict:
+        for id, info in taxid_dict.items():
+            if str(id) in gap_dict:
+                if gap_dict[str(id)] == "SPECIES":
+                    taxid_dict[id][1] = taxid_dict[id][0]
+                    taxid_dict[id][0] = ""
+                elif gap_dict[str(id)] == "GENUS":
+                    taxid_dict[id][2] = taxid_dict[id][0]
+                    taxid_dict[id][0] = ""
+                elif gap_dict[str(id)] == "FAMILY":
+                    taxid_dict[id][3] = taxid_dict[id][0]
+                    taxid_dict[id][0] = ""
+
     dict_keys = input_dict.keys()
 
     output_dict[ 'NoID' ] = list()
@@ -192,7 +206,6 @@ def oligo_to_tax( input_dict, tax_data_file, gap_dict = None ):
 
             else:
                 output_dict[ 'NoID' ].append( current )
-
     return output_dict, taxid_dict
 
 def remove_loc_markers( input_str ):
@@ -229,30 +242,33 @@ def create_oligo_centric_table( tax_dict, map_dict, taxid_dict, gap_dict = None 
               )
     oligo_names     = map_dict.keys()
     num_oligos      = len( oligo_names )
-    species_shared  = set()
+#    species_shared  = set()
 
-    species_total   = set()
-    genus_total     = set()
-    family_total    = set()
-    sequences_total = 0
+#    species_total   = set()
+#    genus_total     = set()
+#    family_total    = set()
+#    sequences_total = 0
 
     for current_oligo in oligo_names:
         current_taxid   = oligo.get_taxid_from_name( current_oligo )
         current_entry = "%s\t%s\t" % ( current_oligo, len( tax_dict[ current_oligo ] ) )
 
         try:
-            if current_taxid:
-                rank_data = gap_dict[ current_taxid ].strip()
+#            if current_taxid:
+#                rank_data = gap_dict[ current_taxid ].strip()
+#                rank_val = oligo.Rank[ rank_data ].value
 
-                rank_val = oligo.Rank[ rank_data ].value
+#             current_species, \
+#             current_genus, \
+#             current_family = get_items_at_rank_from_seqs( map_dict[ current_oligo ],
+#                                                           taxid_dict, 
+#                                                           gap_dict,
+#                                                           oligo.Rank.FAMILY.value
+#                                        )  
 
-            current_species, \
-            current_genus, \
-            current_family = get_items_at_rank_from_seqs( map_dict[ current_oligo ],
-                                                          taxid_dict, 
-                                                          gap_dict,
-                                                          oligo.Rank.FAMILY.value
-                                       )  
+            current_species = set([x[1] for x in tax_dict[current_oligo]])
+            current_genus = set([x[2] for x in tax_dict[current_oligo]])
+            current_family = set([x[3] for x in tax_dict[current_oligo]])
 
             current_species = [ item for item in current_species if len( item ) > 0 ]
             current_genus   = [ item for item in current_genus if len( item ) > 0 ]
@@ -267,12 +283,12 @@ def create_oligo_centric_table( tax_dict, map_dict, taxid_dict, gap_dict = None 
             current_entry += "%s\t" % ",".join( current_genus )
             current_entry += "%s" % ",".join( current_family )
 
-            species_total   |= set( current_species )
-            genus_total     |= set( current_genus )
-            family_total    |= set( current_family )
+#            species_total   |= set( current_species )
+#            genus_total     |= set( current_genus )
+#            family_total    |= set( current_family )
 
             
-            sequences_total += len( map_dict[ current_oligo ] )
+#            sequences_total += len( map_dict[ current_oligo ] )
             out_str         += current_entry + "\n"
 
         except KeyError:
@@ -426,7 +442,7 @@ def parse_gaps( gap_file ):
 
         for line in open_file:
             split_line = line.split( '|' )
-            return_dict[ split_line[ 0 ].strip() ] = split_line[ 1 ]
+            return_dict[ split_line[ 0 ].strip() ] = split_line[ 1 ].strip()
 
         open_file.close() 
 
@@ -434,36 +450,32 @@ def parse_gaps( gap_file ):
         
     return return_val 
 
-def get_items_at_rank_from_seqs( oligo_items,
-                                 taxid_dict,
-                                 gap_dict, 
-                                 oligo_rank
-                               ):
-
-    out_species  = set()
-    out_genera   = set()
-    out_families = set()
-
-    for current_name in oligo_items:
-
-        current_id = oligo.get_taxid_from_name( current_name ).strip()
-        
-        try:
-            if oligo_rank >= oligo.Rank.FAMILY.value:
-                out_families.add( taxid_dict[ int( current_id ) ][ oligo_rank ] )
-                out_genera.add( taxid_dict[ int( current_id ) ][ oligo.Rank.GENUS.value ] )
-                out_species.add( taxid_dict[ int( current_id ) ][ oligo.Rank.SPECIES.value ] )
-
-            elif oligo_rank == oligo.Rank.GENUS.value:
-                out_genera.add( taxid_dict[ int( current_id ) ][ 0 ] )
-
-            elif oligo_rank == oligo.Rank.SPECIES.value:
-                out_species.add( taxid_dict[ int( current_id ) ][ 0 ] )
-
-        except KeyError:
-            pass
-            
-    return out_species, out_genera, out_families
+# def get_items_at_rank_from_seqs( oligo_items, taxid_dict, gap_dict, oligo_rank):
+# 
+#     out_species  = set()
+#     out_genera   = set()
+#     out_families = set()
+# 
+#     for current_name in oligo_items:
+# 
+#         current_id = oligo.get_taxid_from_name( current_name ).strip()
+#         
+#         try:
+#             if oligo_rank >= oligo.Rank.FAMILY.value:
+#                 out_families.add( taxid_dict[ int( current_id ) ][ oligo_rank ] )
+#                 out_genera.add( taxid_dict[ int( current_id ) ][ oligo.Rank.GENUS.value ] )
+#                 out_species.add( taxid_dict[ int( current_id ) ][ oligo.Rank.SPECIES.value ] )
+# 
+#             elif oligo_rank == oligo.Rank.GENUS.value:
+#                 out_genera.add( taxid_dict[ int( current_id ) ][ 0 ] )
+# 
+#             elif oligo_rank == oligo.Rank.SPECIES.value:
+#                 out_species.add( taxid_dict[ int( current_id ) ][ 0 ] )
+# 
+#         except KeyError:
+#             pass
+#             
+#     return out_species, out_genera, out_families
 
 def create_sequence_centric_table( ref_dict, epitope_dict, gap_dict ):
     out_string = ( "Sequence Name\t"
