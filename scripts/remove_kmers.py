c678d81c5b62824250d9d4103b3ff2611bdc3dfc
Bob Costas <finkzane@gmail.com>
2018-11-13 14:26:04 -0700

    added CommandArgError enum class

--- scripts/remove_kmers.py
+++ scripts/remove_kmers.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import argparse                       # for parsing command-line arguments
 import sys                            # for exiting upon failure
+from enum import Enum                 # for error reporting
 import protein_oligo_library as oligo # for operations on oligos/fasta files
+
 
 def main():
 
@@ -11,39 +13,39 @@ def main():
                                                     "of certain categories."
                                     )
     parser.add_argument( '-l', '--lower_bound', type = int, default = 0,
-                       help = "Lower-bound (inclusive) for a value to be "
-                              "considered for output. If a value is less "
-                              "than this, an item is out for consideration. "
-                              "Note that an item must be greater than lower "
-                              "bound and less than upper bound to be considered."
-                              "Also note that lower_bound <= upper_bound."
-                              "To include items that are exactly a value, set "
-                              "upper and lower bound to the same value."
+                         help = "Lower-bound (inclusive) for a value to be "
+                                "considered for output. If a value is less "
+                                "than this, an item is out for consideration. "
+                                "Note that an item must be greater than lower "
+                                "bound and less than upper bound to be considered."
+                                "Also note that lower_bound <= upper_bound."
+                                "To include items that are exactly a value, set "
+                                "upper and lower bound to the same value."
 
                      )
 
     parser.add_argument( '-u', '--upper_bound', type = int, default = 1,
-                       help = "Upper-bound (inclusive) for a value to be "
-                              "considered for output. If a value is greather  "
-                              "than this, it will not be considered for output. "
-                              "Note that lower_bound <= upper_bound."
-                              "To include items that are exactly a value, set "
-                              "upper and lower bound to the same value."
+                         help = "Upper-bound (inclusive) for a value to be "
+                                "considered for output. If a value is greather  "
+                                "than this, it will not be considered for output. "
+                                "Note that lower_bound <= upper_bound."
+                                "To include items that are exactly a value, set "
+                                "upper and lower bound to the same value."
                      )
 
     parser.add_argument( '-t', '--table', type = str,
-                       help = "Oligo-centric table ( as created by map_parse ) "
-                              "to parse. Values in the table, "
-                              "as specified by command-line arguments, "
-                              "are parsed and the oligos that 'fit' the supplied "
-                              "parameters are chosen."
+                         help = "Oligo-centric table ( as created by map_parse ) "
+                                "to parse. Values in the table, "
+                                "as specified by command-line arguments, "
+                                "are parsed and the oligos that 'fit' the supplied "
+                                "parameters are chosen."
                               
 
                      )
 
     parser.add_argument( '-d', '--design', type = str,
-                       help = "Name of the fasta file containing "
-                              "the designed oligos."
+                         help = "Name of the fasta file containing "
+                                "the designed oligos."
                      )
 
     # parse options and arguments
@@ -64,5 +66,13 @@ def main():
 
     # 
 
+class CommandArgError( Enum ):
+    NO_ERROR                  = 0,
+    TABLE_NOT_SUPPLIED_ERROR  = 1,
+    DESIGN_NOT_SUPPLIED_ERROR = 2,
+    INVALID_BOUNDS_ERROR      = 3,
+    TABLE_FILE_IO_ERROR       = 4,
+    DESIGN_FILE_IO_ERROR      = 5
+    
 if __name__ == '__main__':
     main()
