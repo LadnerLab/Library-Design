9771253ff1190e9ad78896f2b42227a77109785f
Bob Costas <finkzane@gmail.com>
2018-11-19 15:28:06 -0700

    creation of file, library imports

---
+++ scripts/swisskb_to_fasta.py
@@ -0,0 +1,14 @@
+import argparse                       # for parsing command-line arguments
+import sys                            # for exiting upon failure
+import protein_oligo_library as oligo # for file operations on sequences/fastas
+
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
