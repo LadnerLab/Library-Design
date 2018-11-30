e8bdeb3a6a3d3d98aba859b22ed2ad71da51057d
Bob Costas <finkzane@gmail.com>
2018-11-30 13:48:49 -0700

    added program options/arguments

--- scripts/one_hundred_rep.py
+++ scripts/one_hundred_rep.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
-
+import argparse                        # For parsing command-line arguments
+import protein_oligo_library as oligo  # for operations on Fasta files
 def main():
-    pass
+    arg_parser = argparse.ArgumentParser( description = "Find and choose the 100% representative sequences from a fasta." )
+
+    arg_parser.add_argument( '-f', '--fasta', help = "Fasta file to read input from" )
+    arg_parser.add_argument( '-r', '--representatives', help = "Output file to write representatives to" )
+    arg_parser.add_argument( '-m', '--map', help = "(Optional) Name of map to write the map of what "
+                                                   "sequences were collapsed under what sequences."
+                           )
+
+    args = arg_parser.parse_args()
 
 
 if __name__ == '__main__':
