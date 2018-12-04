c4107cf10fcc0d2725b4e4aaffe339521386ca53
Bob Costas <finkzane@gmail.com>
2018-12-04 14:46:30 -0700

    added counting number of sequences

--- scripts/100perCreps/one_hundred_reps.c
+++ scripts/100perCreps/one_hundred_reps.c
@@ -16,9 +16,12 @@ int main( int argc, char **argv )
     int num_threads = NUM_THREADS;
     int option      = 0;
     int index       = 0;
+    int num_seqs    = 0;
 
     char in_file[ MAX_NUM_CHARS ];
     char out_file[ MAX_NUM_CHARS ];
+
+    FILE *file = NULL;
 
 
     while( ( option = getopt( argc, argv, ARGS ) ) != -1 )
@@ -36,6 +39,18 @@ int main( int argc, char **argv )
                     
                 }
         }
+
+    file = fopen( in_file, "r" );
+
+    if( !file )
+        {
+            printf( "ERROR: %s could not be found, program will exit...\n", in_file );
+            return EXIT_FAILURE;
+        }
+
+    num_seqs = count_seqs_in_file( file );
+
+    printf( "Num Seqs: %d\n", num_seqs );
 
     return EXIT_SUCCESS;
 }
