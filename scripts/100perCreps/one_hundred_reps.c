735d01f6159bac5f063639144c32f1289d44c7a9
Bob Costas <finkzane@gmail.com>
2018-12-04 14:43:42 -0700

    added argument parsing

--- scripts/100perCreps/one_hundred_reps.c
+++ scripts/100perCreps/one_hundred_reps.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <getopt.h>
+#include <string.h>
 
 #include "protein_oligo_library.h"
 #include "array_list.h"
 
-int main( int argv, char **argv )
-{
+const int NUM_THREADS   = 4;
+const int MAX_NUM_CHARS = 256;
+const char *ARGS        = "f:n:";
 
+int main( int argc, char **argv )
+{
+    int num_threads = NUM_THREADS;
+    int option      = 0;
+    int index       = 0;
+
+    char in_file[ MAX_NUM_CHARS ];
+    char out_file[ MAX_NUM_CHARS ];
+
+
+    while( ( option = getopt( argc, argv, ARGS ) ) != -1 )
+        {
+            switch( option )
+                {
+                    case 'f':
+                        strcpy( in_file, optarg );
+                        strcpy( out_file, optarg );
+                        strcat( out_file, "_out" );
+                        break;
+                    case 'n':
+                        num_threads = atoi( optarg );
+                        break;
+                    
+                }
+        }
+
+    return EXIT_SUCCESS;
 }
