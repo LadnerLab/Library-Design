43806640fb3bcaad758baec751bae3f725e4b7b5
Bob Costas <finkzane@gmail.com>
2018-12-27 09:20:55 -0700

    added imports and main declaration

---
+++ scripts/filenames_and_jobstats.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import argparse                       # for parsing command-line arguments
+import subprocess                     # for getting jobstats data
+import protein_oligo_library as oligo # for getting kmer-counts
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
