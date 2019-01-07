bae20abe54a937fd32daae59d04df762e1982303
Bob Costas <finkzane@gmail.com>
2019-01-07 14:14:46 -0700

    added program dependencies and usage

--- scripts/plot_genome_coverage.py
+++ scripts/plot_genome_coverage.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
-
+from Bio import Entrez           # for efetch
+import argparse                  # for parsing command-line arguments
+from time import sleep           # For limiting requests
+import matplotlib                # for generating charts
+matplotlib.use( 'Agg' )          # Don't want to display charts
+import matplotlib.pyplot as plt  # For generating charts
 
 def main():
-    pass
+
+    # define command-line arguments
+    arg_parser = argparse.ArgumentParser( description = "Download genomes given a list of tax ids and "
+                                                        "accession numbers, plot BLASTp scores "
+                                                        "for each gene."
+
+                                        )
+    arg_parser.add_argument( '-a', '--accession', help = "File containing mappings for TaxId's -> "
+                                                         "Accession numbers."
+                           )
+
+    # parse args
+    args = arg_parser.parse_args()
+
+
+    # verify command-line arguments
 
 
 
