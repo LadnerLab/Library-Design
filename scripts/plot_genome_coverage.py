5b29623b9ac6431e3261bfadf31c7370b9fde215
Bob Costas <finkzane@gmail.com>
2019-01-07 14:37:09 -0700

    added FileParser and AccessionParser classes, implemented parse method

--- scripts/plot_genome_coverage.py
+++ scripts/plot_genome_coverage.py
@@ -5,6 +5,8 @@ from time import sleep           # For limiting requests
 import matplotlib                # for generating charts
 matplotlib.use( 'Agg' )          # Don't want to display charts
 import matplotlib.pyplot as plt  # For generating charts
+import sys                       # for handling errors
+import os                        # for directory operations
 
 def main():
 
@@ -24,7 +26,48 @@ def main():
 
     # verify command-line arguments
 
+    # parse accession-taxId file
 
 
+
+
+class FileParser:
+    def __init__( self, filename = None ):
+        self._filename    = filename
+        self._parsed_data = None
+
+    def parse( self ):
+        pass
+
+class AccessionParser( FileParser ):
+    def __init__( self, filename = None ):
+        super().__init__( filename )
+
+    def parse( self, filename = None ):
+        parse_file = ""
+        if filename:
+            parse_file = filename
+        else:
+            parse_file = self._filename
+
+        return self._parse_file( parse_file )
+
+    def _parse_file( self, filename ):
+        data_dict = {}
+        with open( filename, 'r' ) as open_file:
+            for lineno, line in enumerate( open_file ):
+                if lineno: # Skip first line
+                    split_line = line.strip().split( '\t' )
+                    if split_line[ 0 ] not in data_dict:
+                        data_dict[ split_line[ 0 ] ] = set()
+
+                    data_dict[ split_line[ 0 ] ].add( split_line[ 1 ] )
+        return data_dict
+                    
+                        
+            
+        
+
+            
 if __name__ == '__main__':
     main()
