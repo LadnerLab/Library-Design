3f16804176486a62370d61a85c1f029cf1ed7edb
Bob Costas <finkzane@gmail.com>
2019-02-22 16:27:41 -0700

    added model argument

--- scripts/oligo_encoding/encoding_with_nn.py
+++ scripts/oligo_encoding/encoding_with_nn.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+import h2o      # For running models
+import argparse # for parsing command-line args
+import sys
 
 def main():
-    pass
+    arg_parser = argparse.ArgumentParser( description = "Use h2o to select encodings for oligos." )  
+
+    arg_parser.add_argument( '-m', '--model', description = "Trained model that ill be used to 
 
 if __name__ == '__main__':
     main()
