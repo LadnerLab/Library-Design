d2568111475e49c8d1fc7ba5784c491878b583aa
Bob Costas <finkzane@gmail.com>
2019-03-12 10:53:56 -0700

    added command-line args

--- scripts/get_seqs_with_id.py
+++ scripts/get_seqs_with_id.py
@@ -4,6 +4,21 @@ import sys
 
 
 def main():
-    pass
+    argparser = argparse.ArgumentParser( description = "Retrieve all sequences in a file that "
+                                                       "have contain a specified taxonomic id."
+                                       )
+    argparser.add_argument( '-i', '--ids', help = "Name of file containing one "
+                                                  "id per line"
+                          )
+    argparser.add_argument( '-f', '--fasta', help = "Name of fasta file whose sequences "
+                                                    "will be searched for the specified ids."
+                          )
+    argparser.add_argument( '-t', '--tags', help = "Comma-separated list of tags that specify "
+                                                   "taxonomic id filed in sequence names. [OXX]",
+                            default = "OXX"
+                          )
+
+    args = argparser.parse_args()
+                                                          
 if __name__ == '__main__':
     main()
