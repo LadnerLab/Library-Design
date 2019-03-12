03909829f84b7f5c9d2ccd22877460cfd98327da
Bob Costas <finkzane@gmail.com>
2019-03-12 11:06:30 -0700

    added get_id_set method

--- scripts/get_seqs_with_id.py
+++ scripts/get_seqs_with_id.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 import sys
+import protein_oligo_library as oligo
+import re
 
 
 def main():
@@ -13,12 +15,22 @@ def main():
     argparser.add_argument( '-f', '--fasta', help = "Name of fasta file whose sequences "
                                                     "will be searched for the specified ids."
                           )
-    argparser.add_argument( '-t', '--tags', help = "Comma-separated list of tags that specify "
-                                                   "taxonomic id filed in sequence names. [OXX]",
-                            default = "OXX"
-                          )
-
     args = argparser.parse_args()
-                                                          
+
+    seq_dict        = oligo.sequence_dict_from_file( args.fasta )
+    ids_of_interest = get_id_set( args.ids )
+    taxid_dict      = get_tax_ids( seq_dict )
+
+    query_pattern = r'OXX=[0-9]* *,[0-9]* *,[0-9]* *,[0-9]* *'
+            tax_ids = re.search( query_pattern, query ).group()
+
+def get_id_set( filename ):
+    out_set = set()
+    with open( filename, 'r' ) as open_file:
+        for line in open_file:
+            out_set.add( line.strip() )
+    return out_set
+            
+
 if __name__ == '__main__':
     main()
