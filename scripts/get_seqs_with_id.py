3dcf65edc942a5ab7d7dc888b2740e06535ad355
Bob Costas <finkzane@gmail.com>
2019-03-12 11:29:46 -0700

    added output of names

--- scripts/get_seqs_with_id.py
+++ scripts/get_seqs_with_id.py
@@ -20,7 +20,19 @@ def main():
     seq_dict        = oligo.sequence_dict_from_file( args.fasta )
     ids_of_interest = get_id_set( args.ids )
     taxid_dict      = get_tax_ids( seq_dict )
+    target_names    = get_desired_names( taxid_dict, ids_of_interest )
 
+    print( "Number of sequences containing ids: %d." % len( target_names ) )
+
+    for name in target_names:
+        print( ">%s\n%s" % ( name, seq_dict[ name ] ) )
+
+def get_desired_names( id_dict, id_set ):
+    out_names = set()
+
+    for id in id_set:
+        out_names |= id_dict[ id ]
+    return out_names
     
 def get_tax_ids( seq_dict ):
     id_dict = {}
@@ -35,6 +47,7 @@ def get_tax_ids( seq_dict ):
         for id in ids:
             if id not in id_dict:
                 id_dict[ id ] = set()
+            id_dict[ id ].add( name )
     return id_dict
 
 def get_id_set( filename ):
