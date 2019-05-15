723ebdf9afa0d0228f619bba22681b4426823321
Bob Costas <finkzane@gmail.com>
2019-05-15 14:40:48 -0700

    added changing of filename, get_fasta_stats method and FastaStats class

--- scripts/gen_cov_stats.py
+++ scripts/gen_cov_stats.py
@@ -16,11 +16,22 @@ def main():
     args = arg_parser.parse_args()
 
     design_clusters = parse_fasta_dir( args.design_dir )
+    orig_clusters   = parse_fasta_dir( args.cluster_dir )
+
+    kmer_sizes = [ 7, 8, 9, 10, 30 ]
+
+    design_stats = get_fasta_stats( design_clusters, kmer_sizes )
+    orig_stats   = get_fasta_stats( orig_clusters, kmer_sizes )
+
+
 
 class Sequence:
     def __init__( self, name = "", sequence = "" ):
         self.name     = name
         self.sequence = sequence
+    def get_kmers_with_step( self, k, step_size ):
+        return oligo.subset_lists_iter( self.sequence, k, step_size )
+
     def __hash__( self ):
         return hash( self.sequence )
     def __eq__( self, other ):
@@ -52,11 +63,24 @@ class FastaParser:
 
 class Fasta:
     def __init__( self, filename = "", sequences = None ):
-        self.filename = ""
+        self.filename = filename.split( '/' )[ -1 ]
         self.sequences = list() if sequences is None else sequences
 
     def add_seq( self, new_sequence ):
         self.sequences.append( new_sequence )
+
+    def get_sequences( self ):
+        return self.sequences
+class FastaStats:
+    def __init__( self, filename ):
+        self.filename  = filename
+        self.num_kmers = dict()
+
+    def __hash__( self ):
+        return hash( filename )
+
+    def __eq__( self, other ):
+        return self.filename == other.filename 
 
 def parse_fasta_dir( dirname ):
     files      = os.listdir( dirname )
@@ -68,6 +92,26 @@ def parse_fasta_dir( dirname ):
     return out_fastas
         
         
+def get_fasta_stats( clusters, kmer_sizes ):
+    fasta_stats = list()
+    for fasta in clusters:
+        fasta_stats.append( get_fasta_stat( fasta, kmer_sizes ) )
+    return fasta_stats
+
+def get_fasta_stat( fasta, kmer_sizes ):
+    stat = FastaStats( fasta.filename )
+    seqs = fasta.get_sequences()
+
+    for k in kmer_sizes:
+        k_set = set()
+
+        for seq in seqs:
+            k_set |= seq.get_kmers_with_step( k, 1 )
+        stat.num_kmers[ k ] = len( k_set )
+    print( stat.filename, stat.num_kmers )
+    return stat
+            
+    
 
 if __name__ == '__main__':
     main()
