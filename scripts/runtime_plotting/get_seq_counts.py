a2d2efce7e6ed02bdaebd2c5438ee62fda1420c3
Bob Costas <finkzane@gmail.com>
2019-11-19 13:03:27 -0700

    sequence_counts, not kmer_counts returned

--- scripts/runtime_plotting/get_seq_counts.py
+++ scripts/runtime_plotting/get_seq_counts.py
@@ -6,36 +6,31 @@ import sys
 
 def main():
 
-    if len( sys.argv ) != 4:
-        print( "USAGE: get_kmer_counts in_dir k out_file")
+    if len( sys.argv ) != 3:
+        print( f"USAGE: {sys.argv[0]} in_dir out_file")
         sys.exit( 1 )
 
     in_dir = sys.argv[ 1 ]
-    k = int( sys.argv[ 2 ] )
-    out_file = sys.argv[ 3 ]
+    out_file = sys.argv[ 2 ]
 
     in_files = glob( f'{in_dir}/*')
 
     with open( out_file, 'w' ) as out_f:
-        count_kmers_in_dir( out_f, in_files, k )
+        count_sequences_in_dir( out_f, in_files )
 
-def count_kmers_in_dir( out_fh, list_of_files, k ):
+def count_sequences_in_dir( out_fh, list_of_files ):
     
     for fname in list_of_files:
         if os.path.isdir( fname ):
-            count_kmers_in_dir( out_fh, glob( f'{fname}/*' ), k )
+            count_sequences_in_dir( out_fh, glob( f'{fname}/*' ) )
         else:
-            kmer_count = count_kmers_in_file( fname, k )
+            sequence_count = count_sequences_in_file( fname )
 
-            out_fh.write( f'{fname}\t{kmer_count}\n' )
+            out_fh.write( f'{fname}\t{sequence_count}\n' )
 
-def count_kmers_in_file( filename, k ):
+def count_sequences_in_file( filename ):
     names, sequences = oligo.read_fasta_lists( filename )
-    kmers = set()
-
-    for seq in sequences:
-        kmers |= oligo.subset_lists_iter( seq, k, 1 )
-    return len( kmers )
+    return len( sequences )
 
 if __name__ == '__main__':
     main()
