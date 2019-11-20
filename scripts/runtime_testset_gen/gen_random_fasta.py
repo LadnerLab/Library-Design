746f77c643512ef24b2e4dce393f68922d6192ce
Bob Costas <finkzane@gmail.com>
2019-11-20 15:16:48 -0700

    added CLI args

--- scripts/runtime_testset_gen/gen_random_fasta.py
+++ scripts/runtime_testset_gen/gen_random_fasta.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 import protein_oligo_library as oligo
 import argparse
+import random
 
 
 def main():
-    pass
+    argp = argparse.ArgumentParser( description = "Randomly generate a fasta "
+                                                  "containing AA sequences."
+                                  )
+    argp.add_argument( '-f', '--filename' )
+    argp.add_argument( '-l', '--length', type = int )
+    argp.add_argument( '-g', '--generate',
+                       type = int
+                     )
+    argp.add_argument( '-p', '--prefix', help = "The prefix for each "
+                                                "generated sequence. ",
+                       default = "sequence"
+                     )
+
+    args = argp.parse_args()
 
 
 if __name__ == '__main__':
