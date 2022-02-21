8cafc7e218bc1be4178b1aacc7dc859ecfa7f268
vidalrmartinez <vrm55@nau.edu>
2022-02-21 15:44:03 -0700

    Updating C version of 100reps tool to version with explicit output params and updated map output. This version has produced identical output to python version

--- one_hundred_reps/c/one_hundred_reps.c
+++ one_hundred_reps/c/one_hundred_reps.c
@@ -11,7 +11,7 @@
 
 const int NUM_THREADS   = 4;
 const int MAX_NUM_CHARS = 256;
-const char *ARGS        = "f:h::n:m:";
+const char *ARGS        = "f:o:h::n:m:";
 const int SMALL_TABLE_SIZE = 1000;
 
 int seq_compare( const void *a, const void *b );
@@ -27,7 +27,7 @@ int main( int argc, char **argv )
     int num_seqs    = 0;
 
     char in_file[ MAX_NUM_CHARS ];
-    char out_file[ MAX_NUM_CHARS ];
+    char outfile_name[ MAX_NUM_CHARS ];
 
     FILE *file = NULL;
 
@@ -56,25 +56,28 @@ int main( int argc, char **argv )
         {
             switch( option )
                 {
-                    case 'f':
+                    case 'f': // infile_name
                         strcpy( in_file, optarg );
-                        strcpy( out_file, optarg );
-                        strcat( out_file, "_out" );
                         break;
-                    case 'n':
+                    case 'o': // outfile_name
+                        strcpy( outfile_name, optarg );
+                        break;
+                    case 'n': // num_threads
                         num_threads = atoi( optarg );
                         break;
-                    case 'm':
+                    case 'm': // map_file
                         strcpy( map_file, optarg );
                         map_file_included = true;
                         break;
                     case 'h':
-                        printf( "USAGE: one_hundred_reps -f infile_name -n num_threads -m map_file\n" );
-                        printf( "infile_name name of fasta file to parse and collapse. Output will be written "
-                                "to infile_name_out\n" );
-                        printf( "num_threads number of threads to use. Default is 4.\n" );
-                        printf( "map_file Optional, name of file to write out map to, this map will contain a mapping "
-                                "of which sequences were collapsed under which sequences\n" );
+                        printf( "USAGE:\nexecutable: 'one_hundred_reps'\n"
+                                "-f                            Name of fasta file to parse and collapse.\n");
+                        printf( "-o                            Name of the output fasta file.\n" );
+                        printf( "-n                            Number of threads to use. Default is 4.\n" );
+                        printf( "-m                            Optional, name of file to write out map to, this map will\n"
+                                "                              contain a mapping of which sequences were collapsed under\n"
+                                "                              which sequences.\n" );
+                        printf( "-h                            Provides a help message.");
                         return EXIT_SUCCESS;
                     
                     default:
@@ -127,6 +130,7 @@ int main( int argc, char **argv )
 
     if( map_file_included )
         {
+            // map table populated by in_seqs/input fasta
             map_table = malloc( sizeof( hash_table_t ) );
             ht_init( map_table, TABLE_SIZE );
 
@@ -138,14 +142,15 @@ int main( int argc, char **argv )
                     ht_add( map_table, in_seqs[ index ]->name, new_list );
                 }
         }
-
+    // To Do: Rewrite this to use 3 maps: original in_seq map (seq name : sequence), new no dup in_seq map (seq name : sequence), coverage map (cover seq name : list of covered seq names)
     #pragma omp parallel for private( other_table, other_entries, innermost_index, outer_index, inner_index, found, new_list ) \
                              shared( intermed_seqs, in_seqs, num_seqs, map_table ) schedule( dynamic )
     for( outer_index = 0; outer_index < num_seqs; outer_index++ )
         {
             found = false;
-                for( inner_index = num_seqs - 1; inner_index > outer_index; inner_index-- )
+            for( inner_index = num_seqs - 1; inner_index > outer_index; inner_index-- )
                 {
+                    // if pointer to occurrence of second seq in first seq returned
                     if( strstr( in_seqs[ inner_index]->sequence->data,
                                 in_seqs[ outer_index ]->sequence->data
                               )
@@ -155,13 +160,14 @@ int main( int argc, char **argv )
 
                             if( map_table )
                                 {
-
                                     #pragma omp critical
                                     {
+                                        // if not collapsed
                                         if( in_seqs[ outer_index ]->collapsed == 0)
                                             {
-                                               in_seqs[ outer_index ]->collapsed = 1;
-
+                                                // set collapsed
+                                                in_seqs[ outer_index ]->collapsed = 1;
+                                                
                                                 other_table = ht_delete( map_table, in_seqs[ outer_index ]->name );
 
                                                 new_list = ht_find( map_table, in_seqs[ inner_index ]->name );
@@ -175,7 +181,6 @@ int main( int argc, char **argv )
                                             }
                                     }
                                 }
-                            // we found a seq, no need to continue
                             break;
                         }
 
@@ -200,7 +205,7 @@ int main( int argc, char **argv )
             write_map( map_table, map_file );
         }
 
-    write_fastas( (sequence_t**)out_seqs->array_data, out_seqs->size, out_file );
+    write_fastas( (sequence_t**)out_seqs->array_data, out_seqs->size, outfile_name );
 
     return EXIT_SUCCESS;
 }
@@ -223,7 +228,7 @@ void write_map( hash_table_t *table, char *filename )
     uint32_t index       = 0;
     uint32_t inner_index = 0;
 
-    sequence_t *current_data = NULL;
+    // sequence_t *current_data = NULL;
 
     out_file = fopen( filename, "w" );
 
@@ -232,17 +237,20 @@ void write_map( hash_table_t *table, char *filename )
             ht_items = ht_get_items( table );
             for( index = 0; index < table->size; index++ )
                 {
-                    current_arr = ht_items[ index ]->value;
+                    current_arr = ht_items[ index ]->value; // covered seq names
 
                     current_arr_items = ht_get_items( current_arr );
-                    fprintf( out_file, "%s\t", ht_items[ index ]->key );
+                    fprintf( out_file, "%s\t", ++( ht_items[ index ]->key ) ); // output fasta seq name to first column
 
                     if( current_arr->size > 1 )
                         {
-                            for( inner_index = 1; inner_index < current_arr->size; inner_index++ )
+                            for( inner_index = 0; inner_index < current_arr->size; inner_index++ ) // 3rd index, 18136 seq name segfaults. This is the sequence that gets left out and a duplicate of the cover seq name takes its place normally
                                 {
-                                    current_data = current_arr_items[ inner_index ]->value;
-                                    fprintf( out_file, "%s\t", current_data->name );
+                                    if( strstr( current_arr_items[ inner_index ]->key, ht_items[ index ]->key ) == NULL )
+                                        {
+                                            // current_data = current_arr_items[ inner_index ]->value;
+                                            fprintf( out_file, "%s\t", ++(current_arr_items[ inner_index ]->key) );
+                                        }
                                 }
 
                         }
