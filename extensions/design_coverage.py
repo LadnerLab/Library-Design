dde520a603854453ba2b39fd5d534d9121e1639a
Colleen Ung <ung.colleen@gmail.com>
2023-01-03 10:39:05 -0800

    Update design_coverage.py
    - Added column to output file (for per seq coverage) that provides # of unique kmers being checked.
    - Generates a single coverage stats output file instead of one per input file.

--- extensions/design_coverage.py
+++ extensions/design_coverage.py
@@ -11,8 +11,8 @@ from collections import defaultdict
 parser = argparse.ArgumentParser(description='A script that will calculate the coverage of given target(s) within the design. Coverage will be calculated on a per sequence basis and overall.')
 
 parser.add_argument("-k", "--ksize", default=9, type=int, metavar='\b', help="Kmer size to use when calculating coverage [default: 9].")
-#parser.add_argument("-o", "--output", default="coverage_per_seq.tsv", metavar='\b', help="Name of output tsv file with calculated kmer coverage for each sequence in input file. [default: coverage_per_seq.tsv]")
-#parser.add_argument("-s", "--statsoutput", default="coverage_per_seq_stats.txt", metavar='\b', help="Name of output txt file with descriptive statistics. [default: coverage_per_seq_stats.txt]")
+parser.add_argument("-p", "--perseqoutput", default="coverage-per-seq.tsv", metavar='\b', help="Name of output tsv file with calculated kmer coverage for each sequence (> in length than ksize) in input file. [default: coverage-per-seq.tsv]")
+parser.add_argument("-s", "--statsoutput", default="coverage-stats.tsv", metavar='\b', help="Name of output tsv file with descriptive statistics for per seq coverage and calculated overall coverage. [default: coverage-stats.tsv]")
 parser.add_argument("--swCtoS", default=False, action="store_true", help="Use this flag if Cysteine residues were converted to Serine residues in the SW portion of the design.")
 parser.add_argument("-e", "--extensions", default=".fasta, .fna, .ffn, .faa, .frn, .fa", metavar='\b', help="Only target files with these following file extensions will be grabbed. [default: .fasta, .fna, .ffn, .faa, .frn, .fa]")
 
@@ -61,21 +61,27 @@ for targetF in targetPaths:
 		targetseqD[n].append(seqs[c])
 		c+=1
 	
+	#Dictionary will store # of unique kmers in each seq
+	uniquekmerctD= defaultdict(list)
+	
 	#Calculating coverage at a sequence level
 	coverageperseqD= defaultdict(list)
 	for name, s in targetseqD.items():
 		for sequence in s:
 			if args.swCtoS:
-				sequence= sequence.replace("C", "S")
+				if "C" in sequence:
+					sequence= sequence.replace("C", "S")
 			#Creating set of all unique kmers within sequence
 			sSet= kt.kmerSet(sequence, args.ksize, filter=["X"])
-		
+			
 			if len(sSet)>0:
 				xmersCovered= sSet.intersection(designkSet)
 				percentCovered= (len(xmersCovered) / len(sSet))*100
 				coverageperseqD[name].append(float(percentCovered))
+				uniquekmerctD[name].append(len(sSet))
 			else:
-				coverageperseqD[name].append("N/A")
+				coverageperseqD[name].append("NA")
+				uniquekmerctD[name].append("NA")
 
 	#Calculating overall coverage
 	t= kt.kmerSetFasta(targetF, args.ksize, filter=["X"])		#Creating set of all unique kmers within target file
@@ -89,31 +95,37 @@ for targetF in targetPaths:
 	overallcoverage= (intersect/len(targetkSet))*100
 
 	#Writing out tsv file with per seq kmer coverage
-	outputName= "coverage-per-seq.tsv"
-	with open(outputName, "a") as fout:
+	perseqoutputName= str(args.ksize) + "mer-" + args.perseqoutput
+	with open(perseqoutputName, "a") as fout:
 		if ct == 0:
-			header= "Target\tSequence Name\t%dmer coverage in design\n" % args.ksize
+			header= "Target\tSequence Name\t# of unique %dmers\t%dmer coverage in design\n" % (args.ksize,args.ksize)
 			fout.write(header)
 		
 		for name, coverage in coverageperseqD.items():
+			d=0
 			for c in coverage:
-				if c != "N/A":
-					fout.write("%s\t%s\t%.3f\n" % (targetF, name, c))
+				if c == "NA":
+					d+=1
+				else:
+					uniquekmerct= int(uniquekmerctD[name][d])
+					fout.write("%s\t%s\t%d\t%.3f\n" % (targetF, name, uniquekmerct, c))
+					d+=1
 	
 	#Writing out file with descriptive statistics
-	statsoutputName= os.path.splitext(os.path.basename(targetF))[0] + "_coverage-stats.tsv"
+	#statsoutputName= os.path.splitext(os.path.basename(targetF))[0] + "_coverage-stats.tsv"
 	#Preparing coverageperseqD to perform numpy functions
 	coverageperseqL=[]
 	for coverage in coverageperseqD.values():
 		for c in coverage:
-			if c != "N/A":
+			if c != "NA":
 				coverageperseqL.append(c)
 	
-	with open(statsoutputName, "w") as fout:
-		fout.write("Per sequence coverage summary:\n")
-		header= "Maximum\tQ3\tMedian\tQ1\tMinimum\tIQR\tMean"
-		fout.write(header)
-	
+	statsoutputName= str(args.ksize) + "mer-" + args.statsoutput
+	with open(statsoutputName, "a") as fout:
+		if ct == 0:
+			header= "Target\tMaximum*\tQ3*\tMedian*\tQ1*\tMinimum*\tIQR*\tMean*\tOverall coverage\t\t*Per seq %dmer coverage" % args.ksize
+			fout.write(header)
+		
 		maximum= max(coverageperseqL)
 		q3= np.percentile(coverageperseqL, 75, interpolation = 'midpoint')
 		median= np.percentile(coverageperseqL, 50, interpolation = 'midpoint')
@@ -122,9 +134,8 @@ for targetF in targetPaths:
 		IQR= q3-q1
 		mean= np.mean(coverageperseqL)
 	
-		line2= "\n%f\t%f\t%f\t%f\t%f\t%f\t%f\n" % (maximum,q3,median,q1,minimum,IQR,mean)
+		line2= "\n%s\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%.3f" % (targetF,maximum,q3,median,q1,minimum,IQR,mean,overallcoverage)		
 		fout.write(line2)
-		line3= "\nOverall coverage: %.3f" % overallcoverage
-		fout.write(line3)
+
 	ct+=1
 
